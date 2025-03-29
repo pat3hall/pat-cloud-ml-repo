28afb1ce4f4e42c3f06930bb742f98554f557617
pat3hall <phreggh@gmail.com>
2025-03-28 17:26:09 -0700

    A Cloud Guru AWS Certified Developer Associate Course (111 files)
     Adding A Cloud Guru AWS Certified Developer Associate Course (111 files) at: aws-cloud-training/acloudguru-aws-certified-developer-associate-course

---
+++ aws-cloud-training/acloudguru-aws-certified-developer-associate-course/README.md
@@ -0,0 +1,91 @@
+________________________________________
+
+#### A Cloud Guru: AWS Certified Developer - Associate (DVA-C02):
+________________________________________
+The directory tree contains:
+
+- text note files:
+     1. AWS_Certified_Developer_DVA-C02_aCloudGuru.txt
+        - course notes
+
+- course labs (under labs directory)
+
+    - 04_05_s3_static_website_lab
+
+    - 04_15_athena_demo_lab
+
+    - 05_03_lambda_fcn_lab
+
+    - 05_05_serverless_website_lab
+
+    - 05_06_lambda_version_control_lab
+
+    - 05_11_Serverless_app_lab
+
+    - 05_12_Serverless_website_lab
+
+    - 05_16_lambda_env_lab
+
+    - 05_20_lambda_destinations_lab
+
+    - 05_27_understanding_x-ray_lab
+
+    - 06_02_creating_dynamodb_table_lab
+
+    - 06_06_dynamodb_scan_vs_query_lab
+
+    - 06_11_dynamodb_ttl_lab
+
+    - 07_04_kms_api_calls_lab
+
+    - 08_05_trigger_lambda_from_sqs_lab
+
+    - 08_09_setting_up_kinesis_stream_lab
+
+    - 08_13_elastic_beanstalk_demo_lab
+
+    - 08_14_elastic_beanstalk_updating_demo_lab
+
+    - 09_01_what_is_ci_cd_lab
+
+    - 09_08_deploying_app_using_codedeploy_lab
+
+    - 09_10_codepipeline_demo_lab
+
+    - 09_12_codepipeline_with_manual_approval_lab
+
+    - 09_15_codeartifact_demo_lab
+
+    - 09_18_cloudformation_demo_lab
+
+    - 09_19_cloudformation_dynamodb_lab
+
+    - 09_20_cloudformation_exporting_stack_values_lab
+
+    - 09_22_cloudformation_sam_demo_lab
+
+    - 09_24_cloudformation_nested_stacks_lab
+
+    - 09_26_cdk_demo_lab
+
+    - 09_28_amplify_demo_lab
+
+    - 10_02_cognito_user_pools_demo_lab
+
+    - 10_03_cognito_identity_pools_demo_lab
+
+    - 10_08_cross_account_access_demo_lab
+
+    - 11_02_cloudwatch_demo_lab
+
+    - 11_07_cloudwatch_actions_demo_lab
+
+________________________________________
+
+- course demos (under demos directory)
+
+    - 04_09_CORS_demo
+
+    - 04_11_configuring_cloudfront_demo
+
+________________________________________
---
+++ aws-cloud-training/acloudguru-aws-certified-developer-associate-course/labs/11_07_cloudwatch_actions_demo_lab/publish_metrics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# update the timestamp to today's date and an appropriate time using the following format YYYY-MM-DD HH:MM:SS
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:05:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:06:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:07:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:08:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:09:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:10:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:11:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:12:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:13:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:14:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:15:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:16:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:17:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:18:00.000Z 
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:19:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:20:00.000Z
+aws cloudwatch put-metric-data --metric-name CriticalError --namespace MyService --value 1 --timestamp 2023-07-17T22:21:00.000Z
